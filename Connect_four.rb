2a3433cac11d0d4565ca60e5681acd76fb55c419
Matt Miller <mattmiller0989@gmail.com>
2020-04-25 17:58:42 -0500

    Added respec testing for add disk method

--- Connect_four.rb
+++ Connect_four.rb
@@ -1,6 +1,6 @@
 
 class Game_board
-
+    attr_reader :curr_board
     def initialize
         @curr_board=create_board
     end
@@ -17,4 +17,31 @@ class Game_board
                 
          return board
     end
+
+    def add_disk(column,color)
+        col_arr=[]
+        valid_move=false
+        
+        row=7
+        while row>0 && !valid_move
+            cell=@curr_board[row][column]
+            if cell == " "
+                @curr_board[row][column]=color
+                valid_move=true
+            end
+            
+            row-=1
+        end
+
+        if !valid_move
+            puts "Not a valid move"
+        end
+
+        # print @curr_board
+        
+        
+    end
 end
+
+# my_board=Game_board.new
+# my_board.add_disk(1,'R')
--- spec/Connect_spec.rb
+++ spec/Connect_spec.rb
@@ -1,12 +1,8 @@
 require_relative '../Connect_four'
 
-describe "#create_board" do
-    before(:each) do
-        @test_game=Game_board.new
-    end
 
-    it "Creates an empty 6x7 game board that is numbered on the bottomn" do
-         board=[[" "," "," "," "," "," "," "],
+describe Game_board do
+    orig_board=[[" "," "," "," "," "," "," "],
                 [" "," "," "," "," "," "," "],
                 [" "," "," "," "," "," "," "],
                 [" "," "," "," "," "," "," "],
@@ -14,6 +10,86 @@ describe "#create_board" do
                 [" "," "," "," "," "," "," "],
                 [" "," "," "," "," "," "," "],
                 ["0","1","2","3","4","5","6"]]
-        expect(@test_game.create_board).to eql(board)
+    before(:each) do
+        @test_game=Game_board.new
     end
+    
+    describe "#create_board" do
+         it "Creates an empty 6x7 game board that is numbered on the bottomn" do
+            board=[[" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+            expect(@test_game.create_board).to eql(board)
+        end
+    end
+    describe '#add_disk' do
+        it "Puts a red disk onto an empty column" do
+             board=[[" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+            
+            # subject { @test_game.add_disk(1,'R')}
+             @test_game.add_disk(1,'R')
+            expect(@test_game.curr_board).to eql board 
+        end
+        it "Puts a blue disk into another column" do
+            board=[[" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" ","R"," ","B"," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+           
+           # subject { @test_game.add_disk(1,'R')}
+            @test_game.add_disk(1,'R')
+            @test_game.add_disk(3,'B')
+           expect(@test_game.curr_board).to eql board 
+       end
+       it "Puts a Red disk into another column" do
+        board=[[" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" ","R","R","B"," "," "," "],
+               ["0","1","2","3","4","5","6"]]
+       
+       # subject { @test_game.add_disk(1,'R')}
+        @test_game.add_disk(1,'R')
+        @test_game.add_disk(3,'B')
+        @test_game.add_disk(2,'R')
+       expect(@test_game.curr_board).to eql board 
+   end
+       it "Stacks an additional red disk on top of another" do
+        board=[[" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" "," "," "," "," "," "," "],
+               [" ","R"," "," "," "," "," "],
+               [" ","R"," ","B"," "," "," "],
+               ["0","1","2","3","4","5","6"]]
+       
+       
+        @test_game.add_disk(1,'R')
+        @test_game.add_disk(3,'B')
+        @test_game.add_disk(1,'R')
+       expect(@test_game.curr_board).to eql board 
+   end
+    end
+    
+                
 end
