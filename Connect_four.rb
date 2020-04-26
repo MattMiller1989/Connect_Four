19e4a594366cd2f5636d45647be74786a0222c5c
Matt Miller <mattmiller0989@gmail.com>
2020-04-26 18:23:32 -0500

    Implemented a method for checking the winner with proper testing

--- Connect_four.rb
+++ Connect_four.rb
@@ -1,6 +1,6 @@
 
 class Game_board
-    attr_reader :curr_board
+    attr_accessor :curr_board
     def initialize
         @curr_board=create_board
     end
@@ -23,7 +23,7 @@ class Game_board
         valid_move=false
         
         row=7
-        while row>0 && !valid_move
+        while row>=0 && !valid_move
             cell=@curr_board[row][column]
             if cell == " "
                 @curr_board[row][column]=color
@@ -34,14 +34,127 @@ class Game_board
         end
 
         if !valid_move
-            puts "Not a valid move"
+            # return "Not a valid move"
+            return false
+        else
+            return true
         end
 
         # print @curr_board
         
         
     end
+    def check_for_winner(color)
+        found =false
+
+        for r in 0..6
+
+            for c in 0..6
+                #puts "r:#{r} c:#{c}"
+                if @curr_board[r][c]==color
+                    dir=find_direction(r,c,color)
+                    puts "r:#{r} c:#{c} dir:#{dir}"
+                    if dir
+                         return true
+                    end
+                end
+            end
+        end
+
+        return found
+
+
+    end
+    def find_direction(r,c,color)
+        found = false
+        
+         win_arr=[]
+        
+        
+        if r<=2
+            s_arr=[@curr_board[r+1][c],@curr_board[r+2][c],@curr_board[r+3][c]]
+            win_arr.push(s_arr)
+            
+            if c<=3
+                se_arr=[@curr_board[r+1][c+1],@curr_board[r+2][c+2],@curr_board[r+3][c+3]] 
+                win_arr.push(se_arr)
+                
+            end
+            if c>=3
+                sw_arr=[@curr_board[r+1][c-1],@curr_board[r+2][c-2],@curr_board[r+3][c-3]]
+                win_arr.push(sw_arr)
+                
+
+            end
+        end
+        
+        if r>=3
+            n_arr=[@curr_board[r-1][c],@curr_board[r-2][c],@curr_board[r-3][c]]
+            
+            win_arr.push(n_arr)
+            
+            if c<=3
+                ne_arr=[@curr_board[r-1][c+1],@curr_board[r-2][c+2],@curr_board[r-3][c+3]]
+                win_arr.push(ne_arr)
+                
+                
+            end
+            if c>=3
+                nw_arr=[@curr_board[r-1][c-1],@curr_board[r-2][c-2],@curr_board[r-3][c-3]]
+                win_arr.push(nw_arr)
+                
+            end
+
+        end
+        
+        if c<=3
+            e_arr=[@curr_board[r][c+1],@curr_board[r][c+2],@curr_board[r][c+3]]
+            win_arr.push(e_arr)
+            
+        end
+        if c>=3
+            w_arr=[@curr_board[r][c-1],@curr_board[r][c-2],@curr_board[r][c-3]]
+            win_arr.push(w_arr)
+            
+        end
+
+        
+            
+        win_arr.each do |e|
+            
+            if e.all? {|a| a==color}
+                found =true
+                puts e
+            end
+        end
+
+        return found
+
+    end
+        
+        
+    
 end
 
-# my_board=Game_board.new
+
+board=[[" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," ","R","R","R"," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+
+ my_board=Game_board.new
+ my_board.curr_board=board
 # my_board.add_disk(1,'R')
+# my_board.add_disk(1,'R')
+# my_board.add_disk(1,'R')
+# my_board.add_disk(1,'R')
+# my_board.add_disk(1,'R')
+# my_board.add_disk(1,'R')
+
+#  print my_board.curr_board.class
+ my_board.check_for_winner('R')
--- spec/Connect_spec.rb
+++ spec/Connect_spec.rb
@@ -28,68 +28,363 @@ describe Game_board do
         end
     end
     describe '#add_disk' do
-        it "Puts a red disk onto an empty column" do
-             board=[[" "," "," "," "," "," "," "],
+            it "Puts a red disk onto an empty column" do
+                board=[[" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        ["0","1","2","3","4","5","6"]]
+                
+                
+                @test_game.add_disk(1,'R')
+                expect(@test_game.curr_board).to eql board 
+                end
+            it "Puts a blue disk into another column" do
+                board=[[" "," "," "," "," "," "," "],
                     [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" ","R"," ","B"," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+           
+           
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(3,'B')
+                expect(@test_game.curr_board).to eql board 
+                end
+            it "Puts a Red disk into another column" do
+                board=[[" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" ","R","R","B"," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+       
+            
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(3,'B')
+                @test_game.add_disk(2,'R')
+                expect(@test_game.curr_board).to eql board 
+                end
+            it "Stacks an additional red disk on top of another" do
+                board=[[" "," "," "," "," "," "," "],
                     [" "," "," "," "," "," "," "],
                     [" "," "," "," "," "," "," "],
                     [" "," "," "," "," "," "," "],
                     [" "," "," "," "," "," "," "],
                     [" ","R"," "," "," "," "," "],
+                    [" ","R"," ","B"," "," "," "],
                     ["0","1","2","3","4","5","6"]]
             
-            # subject { @test_game.add_disk(1,'R')}
-             @test_game.add_disk(1,'R')
+            
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(3,'B')
+                @test_game.add_disk(1,'R')
+                expect(@test_game.curr_board).to eql board 
+                end
+            it "Stacks even more red disks on top of the previous" do
+                board=[[" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," ","B"," "," "," "],
+                        ["0","1","2","3","4","5","6"]]
+            
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(3,'B')
+                @test_game.add_disk(1,'R')
+                expect(@test_game.curr_board).to eql board 
+                end
+            it "Stacks all the way to the top of a column" do
+                board=[[" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," ","B"," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+        
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(3,'B')
+                @test_game.add_disk(1,'R')
+                expect(@test_game.curr_board).to eql board 
+                end
+            it "Does not allow overstacking" do
+                board=[[" ","R"," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," ","B"," "," "," "],
+                        ["0","1","2","3","4","5","6"]]
+
+                    @test_game.add_disk(1,'R')
+                    @test_game.add_disk(1,'R')
+                    @test_game.add_disk(1,'R')
+                    @test_game.add_disk(1,'R')
+                    @test_game.add_disk(1,'R')
+                    @test_game.add_disk(1,'R')
+                    @test_game.add_disk(3,'B')
+                    @test_game.add_disk(1,'R')
+                    expect(@test_game.add_disk(1,'R')).to eql false
+                end
+
+            it "Can put blue on top of a red stack" do
+                    board=[[" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" "," "," "," "," "," "," "],
+                        [" ","B"," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," "," "," "," "," "],
+                        [" ","R"," ","B"," "," "," "],
+                        ["0","1","2","3","4","5","6"]]
+            
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(3,'B')
+                @test_game.add_disk(1,'R')
+                @test_game.add_disk(1,'B')
+                expect(@test_game.curr_board).to eql board 
+            end
+            it "Can start a new red stack" do
+                board=[[" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" ","B"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R","R","B"," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+        
+            @test_game.add_disk(1,'R')
+            @test_game.add_disk(1,'R')
+            @test_game.add_disk(3,'B')
+            @test_game.add_disk(1,'R')
+            @test_game.add_disk(1,'B')
+            @test_game.add_disk(2,'R')
             expect(@test_game.curr_board).to eql board 
+            
+            end
+
+        
+    end
+    describe '#check_for_winner' do
+        it "Returns winner with a value of R if there are 4 reds on top of eachother" do
+             board=[[" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    [" ","R"," "," "," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+
+            @test_game.curr_board=board
+            expect(@test_game.check_for_winner('R')).to eql true
         end
-        it "Puts a blue disk into another column" do
+        it "Returns winner with a value of R if there are 4 reds horizontally" do
             board=[[" "," "," "," "," "," "," "],
                    [" "," "," "," "," "," "," "],
                    [" "," "," "," "," "," "," "],
                    [" "," "," "," "," "," "," "],
                    [" "," "," "," "," "," "," "],
                    [" "," "," "," "," "," "," "],
-                   [" ","R"," ","B"," "," "," "],
+                   [" ","R","R","R","R"," "," "],
                    ["0","1","2","3","4","5","6"]]
-           
-           # subject { @test_game.add_disk(1,'R')}
-            @test_game.add_disk(1,'R')
-            @test_game.add_disk(3,'B')
-           expect(@test_game.curr_board).to eql board 
+
+            @test_game.curr_board=board
+           expect(@test_game.check_for_winner('R')).to eql true
+        end
+        it "Returns winner with a value of R if there are 4 reds diagonally" do
+            board=[[" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," ","R"," "," "],
+                   [" "," "," ","R"," "," "," "],
+                   [" "," ","R"," "," "," "," "],
+                   [" ","R"," "," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+           expect(@test_game.check_for_winner('R')).to eql true
+        end
+        it "Returns winner with a value of R if there are 4 reds diagonally the opposite way" do
+            board=[[" ","R"," "," "," "," "," "],
+                   [" "," ","R"," "," "," "," "],
+                   [" "," "," ","R"," "," "," "],
+                   [" "," "," "," ","R"," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+           expect(@test_game.check_for_winner('R')).to eql true
+        end
+        it "Returns winner with a value of R if there are 4 Blues in a row" do
+             board=[[" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," "," "," "," "," "," "],
+                    [" "," ","B","B","B","B"," "],
+                    [" "," "," "," "," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+
+                    @test_game.curr_board=board
+           expect(@test_game.check_for_winner('B')).to eql true
+        end
+        it "Returns winner with a value of R if there are 4 Blues on top of eachother" do
+             board=[[" "," "," "," "," "," "," "],
+                    [" "," "," "," ","B"," "," "],
+                    [" "," "," "," ","B"," "," "],
+                    [" "," "," "," ","B"," "," "],
+                    [" "," "," "," ","B"," "," "],
+                    [" "," "," "," ","B"," "," "],
+                    [" "," "," "," "," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+
+                    @test_game.curr_board=board
+          expect(@test_game.check_for_winner('B')).to eql true
        end
-       it "Puts a Red disk into another column" do
-        board=[[" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" ","R","R","B"," "," "," "],
-               ["0","1","2","3","4","5","6"]]
-       
-       # subject { @test_game.add_disk(1,'R')}
-        @test_game.add_disk(1,'R')
-        @test_game.add_disk(3,'B')
-        @test_game.add_disk(2,'R')
-       expect(@test_game.curr_board).to eql board 
-   end
-       it "Stacks an additional red disk on top of another" do
-        board=[[" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" "," "," "," "," "," "," "],
-               [" ","R"," "," "," "," "," "],
-               [" ","R"," ","B"," "," "," "],
-               ["0","1","2","3","4","5","6"]]
-       
-       
-        @test_game.add_disk(1,'R')
-        @test_game.add_disk(3,'B')
-        @test_game.add_disk(1,'R')
-       expect(@test_game.curr_board).to eql board 
-   end
+        it "Makes sure to interupt non-consecutive disks" do
+             board=[[" "," "," "," "," "," "," "],
+                    [" "," ","R"," "," "," "," "],
+                    [" "," ","R"," "," "," "," "],
+                    [" "," ","R"," "," "," "," "],
+                    [" "," ","R"," "," "," "," "],
+                    [" "," ","B"," "," "," "," "],
+                    [" "," ","R"," "," "," "," "],
+                    ["0","1","2","3","4","5","6"]]
+
+                    @test_game.curr_board=board
+        expect(@test_game.check_for_winner('R')).to eql true
+        end
+        it "Checks for false positives" do
+            board=[[" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," ","R"," "," "," "," "],
+                   [" "," ","R"," "," "," "," "],
+                   [" "," ","B"," "," "," "," "],
+                   [" "," ","R"," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+        expect(@test_game.check_for_winner('R')).to eql false
+        end
+        it "Checks for false positives" do
+            board=[[" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," ","R"," "," "," "," "],
+                   [" "," ","R"," "," "," "," "],
+                   [" "," ","B"," "," "," "," "],
+                   [" "," ","R"," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+        expect(@test_game.check_for_winner('B')).to eql false
+        end
+        it "Checks for win conditions on the border of the board" do
+            board=[[" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," ","B","B","B","B"],
+                   [" "," "," "," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+        expect(@test_game.check_for_winner('B')).to eql true
+        end
+        it "Checks for win conditions on the border of the board" do
+            board=[[" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," ","B"],
+                   [" "," "," "," "," "," ","B"],
+                   [" "," "," "," "," "," ","B"],
+                   [" "," "," "," "," "," ","B"],
+                   [" "," "," "," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+        expect(@test_game.check_for_winner('B')).to eql true
+        end
+        it "Checks for win conditions on the top border of the board" do
+            board=[[" "," "," ","B","B","B","B"],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+        expect(@test_game.check_for_winner('B')).to eql true
+        end
+        it "Checks for win conditions on the bottom border of the board" do
+            board=[[" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," "," "," "," "," "," "],
+                   [" "," ","B","B","B","B"," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+        expect(@test_game.check_for_winner('B')).to eql true
+        end
+        it "Checks for win conditions on the left border of the board" do
+            board=[[" "," "," "," "," "," "," "],
+                   ["B"," "," "," "," "," "," "],
+                   ["B"," "," "," "," "," "," "],
+                   ["B"," "," "," "," "," "," "],
+                   ["B"," "," "," "," "," "," "],
+                   ["R"," "," "," "," "," "," "],
+                   ["R"," "," "," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+        expect(@test_game.check_for_winner('B')).to eql true
+        end
+        it "Checks false positives with a ton of values" do
+            board=[[" "," "," "," "," "," "," "],
+                   ["B"," "," "," "," "," "," "],
+                   ["R","R"," "," "," "," "," "],
+                   ["B"," "," "," "," "," "," "],
+                   ["B"," "," "," "," "," "," "],
+                   ["R"," "," "," "," "," "," "],
+                   ["R"," "," "," "," "," "," "],
+                   ["0","1","2","3","4","5","6"]]
+
+                   @test_game.curr_board=board
+        expect(@test_game.check_for_winner('B')).to eql false
+        end
+
+
     end
-    
-                
 end
+
+                
+
